ecdd8ad196eedd75771f7fd6db4509a2c963ed29
H3XoRuSH <samontegabriel2001@gmail.com>
2020-07-27 23:25:59 +0800

    Update Traffic Lights.cpp
    Update lang kasi mali submission kanina

--- Sorting and Searching/Traffic Lights.cpp
+++ Sorting and Searching/Traffic Lights.cpp
@@ -34,14 +34,22 @@ int main() {
     set<ll> x;
     x.insert(0);
     x.insert(n);
+    multiset<ll> z;
+    z.insert(n);
     ll ans=0;
     REP(i,0,m-1){
         cin >> a;
-        auto it=x.lower_bound(a);
-        auto it2=x.upper_bound(a);
+        auto it2=x.lower_bound(a);
+        auto it=it2; it--;
+        ll len=*it2-*it; //cout << *it << " " << *it2 << endl;
+        auto it3=z.find(len);
+        z.erase(it3);
+        z.insert(abs(*it-a));
+        z.insert(abs(*it2-a));
         x.insert(a);
-        ans=max({ans,abs(*it-a),abs(*it2-a)});
-        cout << ans << " ";
+        it3=z.end(); it3--;
+        cout << *it3 << " ";
+        
     }
     return 0;
 }
